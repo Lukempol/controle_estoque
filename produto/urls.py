e115be450927838af8c4d827756d41c911cf2c32
Lucas Kemp <lukempol@gmail.com>
2023-03-01 14:12:39 -0300

    Adicionar produtos

--- produto/urls.py
+++ produto/urls.py
@@ -3,4 +3,5 @@ from produto import views
 
 urlpatterns = [
     path('', views.ProdutoView.as_view(), name='produto'),
+    path('adicionar', views.CreateProduto.as_view(), name='adicionar'),
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 from produto.models import Produto
+from .forms import ProdutoForm
 
 class ProdutoView(generic.ListView):
     model = Produto
     queryset = Produto.objects.all()
+
+class CreateProduto(generic.edit.CreateView):
+    form_class = ProdutoForm
+    success_url = '/'
+    template_name = 'produto/create_produto.html'
+    model = Produto
