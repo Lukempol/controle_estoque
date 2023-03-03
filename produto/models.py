0dc180f7a81547ece371a85173a08779a86b1c21
Lucas Kemp <lukempol@gmail.com>
2023-03-03 18:01:28 -0300

    Editando HTML e CSS, renomeando a pasta static

--- produto/models.py
+++ produto/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Produto(models.Model):
     name = models.CharField(max_length=50)
-    descrição = models.TextField()
+    descricao = models.TextField()
     quantidade = models.IntegerField(default=0)
 
 
