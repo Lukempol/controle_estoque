dcdc285667225effe998de7d969a7f2e04553f56
Lucas Kemp <lukempol@gmail.com>
2023-03-03 18:42:55 -0300

    Renomendo pasta static

--- controle_estoque/settings.py
+++ controle_estoque/settings.py
@@ -121,10 +121,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-STATICFILES_DIRS = [BASE_DIR / 'static_files']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-STATIC_ROOT = Path(BASE_DIR / 'static')
-MEDIA_ROOT = Path(BASE_DIR / 'media')
+#MEDIA_URL = '/media/'
+#STATIC_ROOT = Path(BASE_DIR / 'static')
+#MEDIA_ROOT = Path(BASE_DIR / 'media')
