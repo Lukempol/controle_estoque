118dea6cc993eadd7ec798139149f0e4e676a00f
Lucas Kemp <lukempol@gmail.com>
2023-03-06 13:45:28 -0300

    Adicionando produto_detail, linha da tabela clicavel

--- produto/urls.py
+++ produto/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from produto import views
 
 urlpatterns = [
-    path('', views.ProdutoView.as_view(), name='produto'),
+    path('', views.ProdutoListView.as_view(), name='produto_list'),
+    path('<int:pk>/', views.ProdutoDetail, name='produto_detail'),
     path('adicionar', views.CreateProduto.as_view(), name='adicionar'),
 ]
--- produto/views.py
+++ produto/views.py
@@ -3,9 +3,16 @@ from django.views import generic
 from produto.models import Produto
 from .forms import ProdutoForm
 
-class ProdutoView(generic.ListView):
+class ProdutoListView(generic.ListView):
     model = Produto
     queryset = Produto.objects.all()
+
+def ProdutoDetail(request, pk):
+    template_name = 'produto/produto_detail.html'
+    #obj = Produto.objects.get(pk=pk)
+    context = {'object': pk}
+    return render(request, template_name, context)
+    
 
 class CreateProduto(generic.edit.CreateView):
     form_class = ProdutoForm
