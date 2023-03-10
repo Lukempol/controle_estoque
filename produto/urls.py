8a26b75cce0f29ec125b88d9b46cbdc8454aa0b0
Lucas Kemp <lukempol@gmail.com>
2023-03-10 16:54:44 -0300

    Home url

--- produto/urls.py
+++ produto/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from produto import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.ProdutoListView.as_view(), name='produto_list'),
     path('<int:pk>/', views.ProdutoDetail, name='produto_detail'),
-    path('adicionar/', views.CreateProduto.as_view(), name='adicionar'),
-    path('editar/<int:pk>/', views.EditProduto.as_view(), name='editar'),
-    path('deletar/<int:pk>/', views.DeleteProduto.as_view(), name='deletar'),
+    path('adicionar/', login_required(views.CreateProduto.as_view()), name='adicionar'),
+    path('editar/<int:pk>/', login_required(views.EditProduto.as_view()), name='editar'),
+    path('deletar/<int:pk>/', login_required(views.DeleteProduto.as_view()), name='deletar'),
+    path('relatorio/', views.some_view, name='relatorio'),
 ]
