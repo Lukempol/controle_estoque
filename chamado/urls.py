cbe324bca7546db634e312f42f33b0c1f9e7829a
Lucas Kemp <lukempol@gmail.com>
2023-05-22 09:37:58 -0300

    Login requerido para abertura de chamado

--- chamado/urls.py
+++ chamado/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+
 from chamado import views
 
 urlpatterns = [
     path('',  views.ChamadoListView.as_view(), name='chamado'),
-    path('abertura/', views.abreChamado, name='abertura'),
+    path('abertura/',login_required(views.abreChamado), name='abertura'),
     path('<str:pk>/', views.chamadoDetail, name='chamado_detail'),
 ]
